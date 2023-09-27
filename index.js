e434ae29a7a9dfbbab4ca1e8d668cb46362f8377
Diana <olednic79@gmail.com>
2023-09-27 16:07:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const numb = 2
+const str = 'text'
+const bool = true
+const undef = undefined
+const withoutVar = null
+const obj = {}
+obj2 = {
+nume: 'nume',
+prenume: 'prenume',
+hobby: 'hobby',
+varsta: 'varsta',
+profesie: 'profesie'
+} 
+
+var a = 24
+var b = 12
+var c = 7
+
+
+console.log(typeof numb)
+console.log(typeof str)
+console.log(typeof bool)
+console.log(typeof undef)
+console.log(typeof withoutVar)
+console.log(typeof obj)
+console.log(a + b + c)
+console.log(a == b == c)
+console.log(a = b = c)
+console.log(c > b)
+console.log(a < b)
+console.log(a >= c)
+console.log(c <= b)
+console.log(numb === 2)
+console.log(bool !== true)
+console.log('Hello ' + 'World')
+console.log(obj2)
+
